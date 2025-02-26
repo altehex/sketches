70515e25ae4834b1dc5e83590f5215b97d519c4d
altehex <ivanronalddobsky@yandex.ru>
2025-02-27 04:18:44 +0500

    Added sub and add for NumberSystems.hs

--- haskell_sketches/NumberSystems.hs
+++ haskell_sketches/NumberSystems.hs
@@ -48,9 +48,9 @@ epsilon = 0.001
 fromBase10D :: Double -> Double -> String
 fromBase10D x base =
   let fx = (x - (fromIntegral $ floor x))
-  in (fromBase10 (floor x) (floor base))
-  ++ "."
-  ++ (convertFract (base*fx) base)
+  in    (fromBase10 (floor x) (floor base))
+     ++ "."
+     ++ (convertFract (base*fx) base)
   where
     convertFract y b
       | fy <= epsilon = [toLetter (floor y)]
@@ -72,3 +72,66 @@ toBase10D s base =
        intFract ('.':cs) = ("", cs)
        intFract (c:cs) =
          let (is, fs) = intFract cs in ((c:is), fs)
+
+
+add :: String -> String -> Int -> String
+add ('-':x) y       base = sub y x base
+add x       ('-':y) base = sub x y base
+add ('-':x) ('-':y) base = ('-' : add x y base)
+add x       y       base =
+  let (rx:rxs) = rev x
+      (ry:rys) = rev y
+      s        = rx <+> ry
+  in rev $ radd s rxs rys base
+  where
+    a <+> b = toNumber a + toNumber b
+
+    radd s' [] [] base
+      | s' < base = [toLetter s']
+      | otherwise = (toLetter (s' - base) : "1")
+
+    radd s' x'@(rx':rxs') [] base
+      | s' < base = (toLetter s'          : x')
+      | otherwise = (toLetter (s' - base) : radd (rx' <+> '1') rxs' [] base)
+
+    radd s' [] y'@(ry':rys') base
+      | s' < base = (toLetter s'          : y')
+      | otherwise = (toLetter (s' - base) : radd (ry' <+> '1') rys' [] base)
+    
+    radd s' (rx':rxs') (ry':rys') base
+      | s' < base = (toLetter s'          : radd (rx' <+> ry')     rxs' rys' base)
+      | otherwise = (toLetter (s' - base) : radd (rx' <+> ry' + 1) rxs' rys' base)
+    
+    rev [x']    = [x']
+    rev (x':xs) = (rev xs)++[x']
+
+
+sub :: String -> String -> Int -> String
+sub ('-':x) ('-':y) base = sub y x base
+sub ('-':x) y       base = ('-' : add x y base)
+sub x       ('-':y) base = add x y base
+sub x       y       base =
+  let (rx:rxs) = rev x
+      (ry:rys) = rev y
+  in stripZeros $ rev $ if (toBase10 x base) > (toBase10 y base) then
+                          rsub (rx <-> ry) rxs rys base
+                        else
+                          (rsub (ry <-> rx) rys rxs base) ++ ['-']
+  where
+    a <-> b = toNumber a - toNumber b
+
+    rsub d' [] [] base = [toLetter d']
+
+    rsub d' x'@(rx':rxs') [] base
+      | d' >= 0   = (toLetter d'          : x')
+      | otherwise = (toLetter (base + d') : rsub (rx' <-> '1') rxs' [] base)
+
+    rsub d' (rx':rxs') (ry':rys') base
+      | d' >= 0   = (toLetter d'          : rsub (rx' <-> ry')     rxs' rys' base)
+      | otherwise = (toLetter (base + d') : rsub (rx' <-> ry' - 1) rxs' rys' base)
+    
+    rev [x']    = [x']
+    rev (x':xs) = (rev xs)++[x']
+
+    stripZeros ('0':as) = stripZeros as
+    stripZeros as       = as
